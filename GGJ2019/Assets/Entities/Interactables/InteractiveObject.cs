d58bd74a11db0a4c57be9611fbed70a2c1496fb4
riktothepast <riktothepast@gmail.com>
2019-01-26 11:18:45 -0600

    Adds mementor manager and interactable objects

---
+++ GGJ2019/Assets/Entities/Interactables/InteractiveObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractiveObject : MonoBehaviour
+{
+    [SerializeField]
+    int id;
+    [SerializeField]
+    string name;
+    [SerializeField]
+    string description;
+    [SerializeField]
+    status currentStatus;
+    [SerializeField]
+    int idToUnlock;
+    public UnityEvent actions;
+
+    public enum status {
+        locked,
+        unlocked
+    }
+
+    private void Start()
+    {
+        MementoManager manager = FindObjectOfType<MementoManager>();
+        if (manager)
+        {
+            manager.AddInteractableObject(this);
+        }
+    }
+
+    public void Interact()
+    {
+        actions.Invoke();
+    }
+
+    public void SetStatus(status status)
+    {
+        currentStatus = status;
+    }
+
+    public int GetID()
+    {
+        return id;
+    }
+
+    public int GetIDToUnlock()
+    {
+        return idToUnlock;
+    }
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}

663c8fbf533a02b102289d5b74f0737356943e60
riktothepast <riktothepast@gmail.com>
2019-01-26 09:22:55 -0600

    characterMovement: Adds basic movement for player character

---
+++ GGJ2019/Assets/Entities/Player/PlayerLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController characterController;
+    [SerializeField]
+    float gravity;
+    [SerializeField]
+    float movementSpeed;
+    Vector3 movementVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        movementVector.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        if (!characterController.isGrounded)
+        {
+            movementVector.y -= gravity * Time.deltaTime;
+        }
+        characterController.Move(movementVector);
+    }
+}

5c471d65fedb32a9a2098b6e09f1bd0c250ab530
Briag Dupont <briagd@yahoo.com>
2019-01-27 10:17:00 -0600

    made presets for audio

--- GGJ2019/Assets/Audio/Synthesizer.cs
+++ GGJ2019/Assets/Audio/Synthesizer.cs
@@ -13,50 +13,77 @@ public class Synthesizer : MonoBehaviour
     //Instrument 1
     //public Transform player;
     public EnemyManager target;
-    double[] notes = { 110,146.83, 164.81, 174.61, 220.00, 233.08, 293.66, 329.63, 349.23, 440,  466.16 };
+    double[] notes = { 110, 146.83, 164.81, 174.61,    220.00, 233.08, 293.66, 329.63, 349.23, 440, 466.16 };
+
+    double[] happyNotes = { 146.83, 164.81, 185.00, 196.00, 220.00, 246.94, 277.18, 293.66, 329.633, 369.99, 392.00 };
+    double[] pentaNotes = { 146.83, 164.81, 185.00, 220.00, 246.94, 293.66, 329.633, 369.99, 220.00, 246.94, 293.66 };
+    double[] nostalgicNotes = { 110, 146.83, 164.81, 174.61, 220.00, 233.08, 293.66, 329.63, 349.23, 440, 466.16 };
     double[] incs = new double[5];
     double[] phases = new double[5];
 
 
-    public double FundFreq = 440.0;
+    public enum Preset {Doom,Happyish,Pacman, Dreamy, Famicom,Custom };
+    public Preset preset;
+
+    private double FundFreq = 440.0;
     private double freq;
-
-    //Oscillators
-    public bool SinOsc = true;
-    public float SinOscAmp = 0.3f;
-    public bool TriOsc = false;
-    public float TriOscAmp = 0.3f;
-    public bool SawOsc = false;
-    public float SawOscAmp = 0.3f;
-    public bool SquareOsc = false;
-    public float SquareOscAmp = 0.3f;
-    public bool subSinOsc = false;
-    public float subOscAmp = 0;
-
     //Sequencer
+    public enum Scale { Nostalgic, Happy, Pentatonic }
+    public Scale myScale;
     private bool hasSeqChanged = false;
     public bool isSeq = false;
+    [Range(0.0f, 32.0f)]
     public double seqFreq = 4;
     private double seqInc;
     private double seqPhase;
     private int currentNote = 0;
 
+    [Range(1, 6)]
+    public int octave = 3;
+    //Oscillators
+    public bool SinOsc = true;
+    [Range(0.0f, 1.0f)]
+    public float SinOscAmp = 0.3f;
+
+
+    public bool TriOsc = false;
+    [Range(0.0f, 1.0f)]
+    public float TriOscAmp = 0.3f;
+    public bool SawOsc = false;
+    [Range(0.0f, 1.0f)]
+    public float SawOscAmp = 0.3f;
+    public bool SquareOsc = false;
+    [Range(0.0f, 1.0f)]
+    public float SquareOscAmp = 0.3f;
+    public bool subSinOsc = false;
+    [Range(0.0f, 1.0f)]
+    public float subOscAmp = 0;
+
+
+
+
+
+    //AMP LFO 
+    public bool isLFOAmp = false;
+    [Range(0.0f, 32.0f)]
+    public double LFOAmpFreq = 0.5;
+    [Range(0.0f, 1.0f)]
+    public float LFOAmpRange = 0.4f;
+    private double LFOAmpInc;
+    private double LFOAmpPhase;
+    public bool isLockFreqToSeq = false;
+
     //Freq LFO
     public bool isLFOFreq = false;
+    [Range(0.0f, 32.0f)]
     public double LFOFreq = 4;
+    [Range(0.0f, 10.0f)]
     public float LFOFreqRange = 0.4f;
     private double LFOInc;
     private double LFOPhase;
 
-    //AMP LFO 
-    public bool isLFOAmp = false;
-    public double LFOAmpFreq = 0.5;
-    public float LFOAmpRange = 0.4f;
-    private double LFOAmpInc;
-    private double LFOAmpPhase;
-
     //game object with message
-    public double distThreshold=3.0;
+    public double distThreshold = 3.0;
     bool isCloseToTarget = false;
 
 
@@ -69,20 +96,20 @@ public class Synthesizer : MonoBehaviour
     private void Update()
     {
 
-        if (target.isChasing){
-            SinOscAmp = 0.4f;
-            SawOsc = true;
-            isLFOFreq = true;
-            isSeq = false;
+        //if (target.isChasing){
+        //    SinOscAmp = 0.4f;
+        //    SawOsc = true;
+        //    isLFOFreq = true;
+        //    isSeq = false;
 
-        } else {
-            SinOscAmp = 0.6f;
-            SawOsc = false;
-            isLFOFreq = false;
-            isSeq = true;
-        }
+        //} else {
+        //    SinOscAmp = 0.6f;
+        //    SawOsc = false;
+        //    isLFOFreq = false;
+        //    isSeq = true;
+        //}
         //double dist = Vector3.Distance(player.position, target.position);
-       
+
         //if (dist < distThreshold && !isCloseToTarget)
         //{
         //    FundFreq = notes[Random.Range(0, 4)];
@@ -100,7 +127,22 @@ public class Synthesizer : MonoBehaviour
         //For theramin effect
         //frequency = fundFrequency * (1.0 + player.normalizedPosX);
         //amp = player.normalizedPosY;
+        if (isLockFreqToSeq)
+        {
+            LFOAmpFreq = seqFreq;
+        }
 
+        if (myScale==Scale.Nostalgic){
+            for (int i = 0; i < notes.Length; i++) notes[i] = octave/3.0f*happyNotes[i];
+
+        } else if (myScale == Scale.Happy){
+            for (int i = 0; i < notes.Length; i++) notes[i] = octave / 3.0f * nostalgicNotes[i];
+        }
+        else if (myScale == Scale.Pentatonic)
+        {
+            for (int i = 0; i < notes.Length; i++) notes[i] = octave / 3.0f * pentaNotes[i];
+        }
+        setPreset();
     }
 
 
@@ -108,7 +150,7 @@ public class Synthesizer : MonoBehaviour
     {
 
         Instrument1Inc();
-        for (int i = 0; i < data.Length; i+= channels)
+        for (int i = 0; i < data.Length; i += channels)
         {
             Instrument1UpdatePhases();
             data[i] = amp * Instrument1Sig();
@@ -124,11 +166,16 @@ public class Synthesizer : MonoBehaviour
     }
 
     //Instrument 1
-    private void Instrument1Inc(){
+    private void Instrument1Inc()
+    {
+
+
         if (isLFOFreq)
         {
             freq = SinRanged(LFOPhase, (float)notes[currentNote] * (1.0f - LFOFreqRange), (float)notes[currentNote] * (1.0f + LFOFreqRange));
-        } else {
+        }
+        else
+        {
             freq = notes[currentNote];
         }
         incs[0] = freq * freqMultiplier;
@@ -142,7 +189,8 @@ public class Synthesizer : MonoBehaviour
         seqInc = seqFreq * freqMultiplier;
     }
 
-    private void Instrument1UpdatePhases(){
+    private void Instrument1UpdatePhases()
+    {
         for (int i = 0; i < incs.Length; i++)
         {
             phases[i] += incs[i];
@@ -153,7 +201,8 @@ public class Synthesizer : MonoBehaviour
     }
 
     //Change note
-    private void ChangeNote(){
+    private void ChangeNote()
+    {
         float s = Mathf.Sin((float)seqPhase);
         float r = s * 0.5f + 0.5f;
 
@@ -169,9 +218,21 @@ public class Synthesizer : MonoBehaviour
             hasSeqChanged = !hasSeqChanged;
 
         }
-        if (currentNote>notes.Length-1){
+        if (currentNote > notes.Length - 1)
+        {
             currentNote = 0;
         }
+    }
+
+    private float normalizeAmp()
+    {
+        float nAmp = 0.00001f;
+        if (SinOsc) nAmp += SinOscAmp;
+        if (TriOsc) nAmp += TriOscAmp;
+        if (SquareOsc) nAmp += SquareOscAmp;
+        if (SawOsc) nAmp += SawOscAmp;
+
+        return nAmp;
     }
 
     //Addittive synth section
@@ -192,38 +253,170 @@ public class Synthesizer : MonoBehaviour
             sig *= SquareOscAmp;
         }
 
-        if (TriOsc){
+        if (TriOsc)
+        {
             sig += (float)(0.5 * Mathf.PingPong((float)phases[0], 1.0f));
 
             sig *= TriOscAmp;
         }
 
-        if(SawOsc){
+        if (SawOsc)
+        {
             sig += (float)(0.5 * Mathf.Sin((float)phases[0])
                        + 0.25 * Mathf.Sin((float)phases[1]) +
-                     0.125* Mathf.Sin((float)phases[2]) + 0.0625* Mathf.Sin((float)phases[3]));
+                     0.125 * Mathf.Sin((float)phases[2]) + 0.0625 * Mathf.Sin((float)phases[3]));
             sig *= SawOscAmp;
         }
 
-        if (SinOsc){
+        if (SinOsc)
+        {
             sig += SinOscAmp * Mathf.Sin((float)phases[0]);
 
         }
 
-        if (subSinOsc){
-            sig += (float)(0.5 * Mathf.Sin((float)phases[4]) * subOscAmp);
+        if (subSinOsc)
+        {
+            sig += (float)(subOscAmp * Mathf.Sin((float)phases[4]) * subOscAmp);
         }
 
-        if(isLFOAmp){
-            sig *= SinRanged((float)LFOAmpPhase, 0.0f, 1.0f);
+        sig /= normalizeAmp();
+
+        if (isLFOAmp)
+        {
+            sig *= SinRanged((float)LFOAmpPhase + Mathf.PI, 1.0f-LFOAmpRange, 1.0f);
         }
 
         return sig;
     }
 
-    private float SinRanged(double value, float min, float max){
+    private float SinRanged(double value, float min, float max)
+    {
 
-        return (max+min + (max-min)*Mathf.Sin((float)value))/2.0f;
+        return (max + min + (max - min) * Mathf.Sin((float)value)) / 2.0f;
+    }
+
+
+
+    void setPreset()
+    {
+        if (preset.Equals(Preset.Doom)){
+            myScale = Scale.Nostalgic;
+            isSeq = true;
+            seqFreq = 1;
+            octave = 1;
+            SinOsc = false;
+            SinOscAmp = 0.3f;
+            TriOsc = false;
+            TriOscAmp = 0.3f;
+            SawOsc = true;
+            SawOscAmp = 0.54f;
+            SquareOsc = true;
+            SquareOscAmp = 0.15f;
+            subSinOsc = true;
+            subOscAmp = 0.27f;
+            isLFOAmp = true;
+            LFOAmpFreq = 4;
+            LFOAmpRange = 0.46f;
+            isLockFreqToSeq = false;
+            isLFOFreq = false;
+            LFOFreq = 4;
+            LFOFreqRange = 0.4f;
+        } else if (preset.Equals(Preset.Happyish))
+        {
+            myScale = Scale.Pentatonic;
+            isSeq = true;
+            seqFreq = 6;
+            octave = 3;
+            SinOsc = true;
+            SinOscAmp = 0.3f;
+            TriOsc = true;
+            TriOscAmp = 0.53f;
+            SawOsc = false;
+            SawOscAmp = 0.54f;
+            SquareOsc = false;
+            SquareOscAmp = 0.15f;
+            subSinOsc = true;
+            subOscAmp = 0.27f;
+            isLFOAmp = true;
+            LFOAmpFreq = 6;
+            LFOAmpRange = 0.72f;
+            isLockFreqToSeq = true;
+            isLFOFreq = false;
+            LFOFreq = 4;
+            LFOFreqRange = 0.4f;
+        }
+        else if (preset.Equals(Preset.Pacman))
+        {
+            myScale = Scale.Pentatonic;
+            isSeq = false;
+            seqFreq = 6;
+            octave = 3;
+            SinOsc = true;
+            SinOscAmp = 0.13f;
+            TriOsc = true;
+            TriOscAmp = 0.8f;
+            SawOsc = false;
+            SawOscAmp = 0.54f;
+            SquareOsc = false;
+            SquareOscAmp = 0.15f;
+            subSinOsc = true;
+            subOscAmp = 0.65f;
+            isLFOAmp = false;
+            LFOAmpFreq = 6;
+            LFOAmpRange = 0.72f;
+            isLockFreqToSeq = true;
+            isLFOFreq = true;
+            LFOFreq = 5;
+            LFOFreqRange = 0.3f;
+        }
+        else if (preset.Equals(Preset.Dreamy))
+        {
+            myScale = Scale.Happy;
+            isSeq = true;
+            seqFreq = 4;
+            octave = 2;
+            SinOsc = true;
+            SinOscAmp = 0.5f;
+            TriOsc = true;
+            TriOscAmp = 0.18f;
+            SawOsc = true;
+            SawOscAmp = 0.68f;
+            SquareOsc = false;
+            SquareOscAmp = 0.15f;
+            subSinOsc = false;
+            subOscAmp = 0.65f;
+            isLFOAmp = true;
+            LFOAmpFreq = 1;
+            LFOAmpRange = 0.78f;
+            isLockFreqToSeq = false;
+            isLFOFreq = true;
+            LFOFreq = 0.1;
+            LFOFreqRange = 0.1f;
+        }
+        else if (preset.Equals(Preset.Famicom))
+        {
+            myScale = Scale.Pentatonic;
+            isSeq = true;
+            seqFreq = 7.4;
+            octave = 4;
+            SinOsc = true;
+            SinOscAmp = 0.53f;
+            TriOsc = false;
+            TriOscAmp = 0.18f;
+            SawOsc = false;
+            SawOscAmp = 0.68f;
+            SquareOsc = true;
+            SquareOscAmp = 0.15f;
+            subSinOsc = false;
+            subOscAmp = 0.65f;
+            isLFOAmp = true;
+            LFOAmpFreq = 3.7;
+            LFOAmpRange = 0.56f;
+            isLockFreqToSeq = false;
+            isLFOFreq = false;
+            LFOFreq = 0.1;
+            LFOFreqRange = 0.1f;
+        }
     }
 
 

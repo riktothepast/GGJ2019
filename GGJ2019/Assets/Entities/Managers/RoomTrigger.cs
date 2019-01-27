5b90b5eb5de49357fe9480c66972a240a3cb1674
riktothepast <riktothepast@gmail.com>
2019-01-27 10:03:56 -0600

    Adds cameras to 1st floor and room triggers

---
+++ GGJ2019/Assets/Entities/Managers/RoomTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> objectsToActivate;
+    [SerializeField]
+    List<GameObject> objectsToDeactivate;
+
+    void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject obj in objectsToActivate) {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in objectsToDeactivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
